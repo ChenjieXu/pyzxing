0389ad3649da85c51b758c43e3a97cfd005c3598
Chenjie Xu <cxuscience@gmail.com>
2020-05-31 15:34:44 +0800

    support read multiple barcodes in a picture

--- src/test_decode.py
+++ src/test_decode.py
@@ -11,35 +11,35 @@ class TestBarCodeReaderDecode(unittest.TestCase):
         result = self.reader.decode(basename + '.png')
         with open(basename+'.txt', 'r') as fp:
             gt = fp.readline().strip()
-        self.assertEqual(result['parsed'], gt)
+        self.assertEqual(result[0]['parsed'], gt)
 
     def test_code39(self):
         basename = 'src/resources/code39'
         result = self.reader.decode(basename + '.png')
         with open(basename+'.txt', 'r') as fp:
             gt = fp.readline().strip()
-        self.assertEqual(result['parsed'], gt)
+        self.assertEqual(result[0]['parsed'], gt)
 
     def test_code128(self):
         basename = 'src/resources/code128'
         result = self.reader.decode(basename + '.png')
         with open(basename+'.txt', 'r') as fp:
             gt = fp.readline().strip()
-        self.assertEqual(result['parsed'], gt)
+        self.assertEqual(result[0]['parsed'], gt)
 
     def test_pdf417(self):
         basename = 'src/resources/pdf417'
         result = self.reader.decode(basename + '.png')
         with open(basename+'.txt', 'r') as fp:
             gt = fp.readline().strip()
-        self.assertEqual(result['parsed'], gt)
+        self.assertEqual(result[0]['parsed'], gt)
 
     def test_qrcode(self):
         basename = 'src/resources/qrcode'
         result = self.reader.decode(basename + '.png')
         with open(basename+'.txt', 'r') as fp:
             gt = fp.readline().strip()
-        self.assertEqual(result['parsed'], gt)
+        self.assertEqual(result[0]['parsed'], gt)
 
     def test_nonexistfile(self):
         basename = 'src/resources/nonexistfile'
@@ -49,4 +49,14 @@ class TestBarCodeReaderDecode(unittest.TestCase):
     def test_nobarcodefile(self):
         basename = 'src/resources/ou'
         result = self.reader.decode(basename + '.jpg')
-        self.assertEqual(result, None)
+        self.assertEqual(result[0], None)
+
+    def test_multibarcodes(self):
+        basename = 'src/resources/multibarcodes'
+        results = self.reader.decode(basename + '.jpg')
+        result_string = [result['parsed'] for result in results]
+
+        with open(basename+'.txt', 'r') as fp:
+            gt = [line.strip() for line in fp.readlines()]
+
+        self.assertEqual(set(result_string), set(gt))
