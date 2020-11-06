b8ec352369872fa0b799f3dc0b375392a0be1723
许晨杰 <xuchenjie@baosight.com>
2020-11-06 08:50:49 +0800

    Update code formatting

--- pyzxing/reader.py
+++ pyzxing/reader.py
@@ -1,9 +1,10 @@
-import os
 import ast
 import glob
-import urllib.request
+import os
 import shutil
 import subprocess
+import urllib.request
+
 from joblib import Parallel, delayed
 
 jar_filename = "javase-3.4.1-SNAPSHOT-jar-with-dependencies.jar"
@@ -17,13 +18,11 @@ class BarCodeReader():
 
     def __init__(self):
         """Prepare necessary jar file."""
-        res = glob.glob(
-            jar_path + "javase-*-jar-with-dependencies.jar")
+        res = glob.glob(jar_path + "javase-*-jar-with-dependencies.jar")
         if res:
             self.lib_path = res[0]
         else:
-            print(
-                "ZXing library is not compiled. Use local jar file.")
+            print("ZXing library is not compiled. Use local jar file.")
             download_url = jar_url + jar_filename
             save_path = jar_path + jar_filename
             if not os.path.exists(save_path):
@@ -31,7 +30,8 @@ class BarCodeReader():
                 if not os.path.exists(jar_path):
                     os.makedirs(jar_path)
                 req = urllib.request.Request(download_url)
-                with urllib.request.urlopen(req) as resp, open(save_path, 'wb') as out:
+                with urllib.request.urlopen(req) as resp, open(
+                        save_path, 'wb') as out:
                     shutil.copyfileobj(resp, out)
                 print("Download completed.")
             self.lib_path = save_path
@@ -46,21 +46,27 @@ class BarCodeReader():
             results = self._decode(filenames[0].replace('\\', '/'))
 
         else:
-            results = Parallel(n_jobs=-1)(delayed(self._decode)
-                                          (filename.replace('\\', '/')) for filename in filenames)
+            results = Parallel(n_jobs=-1)(
+                delayed(self._decode)(filename.replace('\\', '/'))
+                for filename in filenames)
 
         return results
 
     def _decode(self, filename):
         cmd = ' '.join([self.command, self.lib_path, filename, '--multi'])
-        (stdout, _) = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, universal_newlines=True, shell=True).communicate()
+        (stdout, _) = subprocess.Popen(cmd,
+                                       stdout=subprocess.PIPE,
+                                       universal_newlines=True,
+                                       shell=True).communicate()
         lines = stdout.splitlines()
-        separator_idx = [i for i in range(
-            len(lines)) if lines[i][:4] == 'file'] + [len(lines)]
+        separator_idx = [
+            i for i in range(len(lines)) if lines[i][:4] == 'file'
+        ] + [len(lines)]
 
-        result = [self._parse_single(
-            lines[separator_idx[i]:separator_idx[i+1]]) for i in range(len(separator_idx)-1)]
+        result = [
+            self._parse_single(lines[separator_idx[i]:separator_idx[i + 1]])
+            for i in range(len(separator_idx) - 1)
+        ]
         return result
 
     @staticmethod
@@ -89,20 +95,21 @@ class BarCodeReader():
             raw_index = find_line_index(lines, "Raw result:")
             parsed_index = find_line_index(lines, "Parsed result:")
             points_index = find_line_index(lines, "Found")
-            
+
             if not raw_index or not parsed_index or not points_index:
                 print("Parse Error!")
                 return lines
-            result['raw'] = lines[raw_index+1:parsed_index]
+            result['raw'] = lines[raw_index + 1:parsed_index]
             result['raw'] = '\n'.join(result['raw'])
-            result['parsed'] = lines[parsed_index+1:points_index]
+            result['parsed'] = lines[parsed_index + 1:points_index]
             result['parsed'] = '\n'.join(result['parsed'])
             # result['points'] = [ast.literal_eval(line[12:]) for line in lines[points_index+1:-1]]
         return result
+
 
 def find_line_index(lines, content):
     for i, line in enumerate(lines):
         if line[:len(content)] == content:
             return i
-    
+
     return None
