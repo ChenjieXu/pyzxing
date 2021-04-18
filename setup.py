681e6a37900963ea29430936e1bb4c0d2b23090e
Chenjie Xu <cxuscience@gmail.com>
2021-04-18 15:14:26 +0800

    release 1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="pyzxing",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/ChenjieXu/pyzxing",
     description="Python wrapper for ZXing Java library",
     long_description=readme,
