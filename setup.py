3a16f15572473a0bc3658db5200fcb6c3f1b5f43
ChenjieXu <cxuscience@gmail.com>
2022-01-17 20:53:31 +0800

    Use navdeep-G/setup.py as template

--- setup.py
+++ setup.py
@@ -1,21 +1,109 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-with open('README.md', encoding="utf8") as f:
-    readme = f.read()
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+VERSION = '1.0.2b'
+NAME = 'pyzxing'
+DESCRIPTION = 'Python wrapper for ZXing Java library.'
+URL = 'https://github.com/ChenjieXu/pyzxing'
+EMAIL = 'cxuscience@gmail.com'
+AUTHOR = 'Chenjie Xu'
+REQUIRES_PYTHON = '>=3.6.0'
+
+# What packages are required for this module to be executed?
 with open('requirements.txt') as f:
     reqs = f.read()
+REQUIRED = reqs.strip().split('\n')
 
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name="pyzxing",
-    version="1.0.2",
-    url="https://github.com/ChenjieXu/pyzxing",
-    description="Python wrapper for ZXing Java library",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    author="Chenjie Xu",
-    author_email="cxuscience@gmail.com",
-    keywords='zxing',
-    packages=find_packages(),
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
     license='MIT',
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -28,5 +116,8 @@ setup(
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    install_requires=reqs.strip().split('\n'),
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
